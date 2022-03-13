8668b21e9219a3c02db6fd937b32cda6620d0006
Alexis Hovorka <alexisspacegirl@gmail.com>
2022-03-13 15:15:37 -0600

    [style] Change fonts, battle view styling

--- src/views/BattleView.vue
+++ src/views/BattleView.vue
@@ -8,34 +8,35 @@
     </div>
     <div v-else>
       <h1>Pokemon Battle!</h1>
-      <PokeStatus :pokemon="opponentIndex" :opponent="true" />
-      <PokeStatus :pokemon="playerIndex" :opponent="false" />
-      <div class="menu">
-        <div v-if="menu == 'main'">
-          <button v-on:click="setMenu('fight', $event)">FIGHT</button>
-          <button v-on:click="setMenu('item')">ITEM</button>
-          <button v-on:click="setMenu('pokemon', $event)">POKEMON</button>
-          <button v-on:click="runAway($event)">RUN</button>
-        </div>
-        <div v-else-if="menu == 'fight'">
-          <div v-for="move in this.$root.$data.pokemon[playerPokemon.name]['moves']" :key="move">
-            <button v-on:click="attack(move, $event)">{{move}}</button>
+      <div class="screen-container">
+        <PokeStatus :pokemon="opponentIndex" :opponent="true" />
+        <PokeStatus :pokemon="playerIndex" :opponent="false" />
+        <div class="menu">
+          <div v-if="menu == 'main'">
+            <button v-on:click="setMenu('fight', $event)">FIGHT</button>
+            <button v-on:click="setMenu('item')">ITEM</button>
+            <button v-on:click="setMenu('pokemon', $event)">PKMN</button>
+            <button v-on:click="runAway($event)">RUN</button>
           </div>
-          <button v-on:click="setMenu('main', $event)">BACK</button>
-        </div>
-        <div v-else-if="menu == 'item'">
-          
-          <button v-on:click="setMenu('main', $event)">BACK</button>
-        </div>
-        <div v-else-if="menu == 'pokemon'">
-          <div v-for="pokemon in playerParty" :key="pokemon.name">
-            <button v-bind:disabled="pokemon.currentHP < 1 || pokemon.name == playerParty[playerIndex].name" v-on:click="switchPokemon('pokemon.name', $event)">{{pokemon.name}}</button>
+          <div v-else-if="menu == 'fight'">
+            <div v-for="move in this.$root.$data.pokemon[playerPokemon.name]['moves']" :key="move">
+              <button v-on:click="attack(move, $event)">{{move}}</button>
+            </div>
+            <button v-on:click="setMenu('main', $event)">BACK</button>
           </div>
-          <button v-on:click="setMenu('main', $event)">BACK</button>
+          <div v-else-if="menu == 'item'">
+            <button v-on:click="setMenu('main', $event)">BACK</button>
+          </div>
+          <div v-else-if="menu == 'pokemon'">
+            <div v-for="pokemon in playerParty" :key="pokemon.name">
+              <button v-bind:disabled="pokemon.currentHP < 1 || pokemon.name == playerParty[playerIndex].name" v-on:click="switchPokemon('pokemon.name', $event)">{{pokemon.name}}</button>
+            </div>
+            <button v-on:click="setMenu('main', $event)">BACK</button>
+          </div>
         </div>
-      </div>
-      <div class="messages">
-        <p v-for="message in messages" :key="message.message">&lt;{{message.source}}&gt;: {{message.message}}</p>
+        <div class="messages">
+          <p v-for="message in messages" :key="message.message">&lt;{{message.source}}&gt;: {{message.message}}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -117,8 +118,24 @@ export default {
 </script>
 
 <style scoped>
+  .screen-container {
+    margin: 0 auto;
+    width: 320px;
+    height: 288px;
+    /*box-shadow: 0 0 0 1px #f00;*/
+  }
+
   button {
-    font-family: 'pkmn_rbygscregular';
+    font-family: "pokemon-font";
+    border: 0;
+    background: 0;
+    cursor: pointer;
+  }
+  button::before {
+    content: "\a0";
+  }
+  button:hover::before {
+    content: "\25b6";
   }
 
   .messages {
